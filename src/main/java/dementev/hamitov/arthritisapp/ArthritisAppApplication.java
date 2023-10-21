928487a64be9d44a55cee7f2ab327ccc29564242
Dmitrii Dementev <dmitrii.dementev@aisa.ru>
2023-10-21 17:23:59 +0300

    spring.boot.actuator

--- src/main/java/dementev/hamitov/arthritisapp/ArthritisAppApplication.java
+++ src/main/java/dementev/hamitov/arthritisapp/ArthritisAppApplication.java
@@ -1,13 +1,17 @@
 package dementev.hamitov.arthritisapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.boot.actuate.info.MapInfoContributor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 @SpringBootApplication
 public class ArthritisAppApplication {
@@ -33,5 +37,18 @@ public class ArthritisAppApplication {
 		messageSource.setDefaultEncoding("UTF-8");
 		return messageSource;
 	}
+
+	@Bean
+	InfoContributor getInfoContributor() {
+		Map<String, Object> details = new HashMap<>();
+		details.put("nameApp", "Atrhritis");
+		details.put("developers", "Dementev Dmitrii and Abulkair Hamitov");
+		details.put("group", "9308");
+		details.put("email", "exmaple@mail.ru");
+		Map<String, Object> wrapper = new HashMap<>();
+		wrapper.put("info", details);
+		return new MapInfoContributor(wrapper);
+	}
+
 
 }
